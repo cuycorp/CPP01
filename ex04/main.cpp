f0fbc62d6b19edbcb28a4318a3dd1126118b5d8e
cuycorp <cuycorp@gmail.com>
2025-09-26 13:20:47 +0200

    wip reads line. Pending:  loop to read an entire file

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,33 +1,99 @@
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <fstream>
+#include "main.hpp"
 
-
-int main(int ac, char **av)
+std::string processLine(std::string line, std::string search,
+	std::string replace)
 {
-    std::string line;
-    std::ifstream inFile;
-    std::string nameOutfile;
-    
-    //A. error handling
-        /*
-        1. wrong No arguments
-        2. infile does not exist or cant be opened
-        3. s1="" and/ s2=""
-        * case for empty file
-        */
-    if (ac != 4)
-        return (std::cout << "Bad arguments" << std::endl, 1);
-    //B. Open file input and set output
-    inFile.open(av[1]);
-    if (!inFile.is_open())
-        return (std::cout << "Could not open input file" << std::endl, 1);
-    nameOutfile = av[1] + ".replace";
-    std::ofstream outFile(nameOutfile);
-    if (!outFile.is_open())
-        return (std::cout << "Could not open output file" << std::endl, 1);
-    //C. Read 
-    //D. Replace words in line
-    //E. Put final line in file
+	int	index;
+	std::string newLine;
+	std::string::size_type occurrence;
+	std::string subString;
+
+	occurrence = 0;
+	index = 0;
+	newLine = "";
+	while (1)
+	{
+		occurrence = line.find(search, index);
+		if (occurrence != std::string::npos)
+		{
+            subString = line.substr(index, occurrence - index);
+			newLine += subString;
+			newLine += replace;
+			index = index + subString.length() + search.length();
+		}
+		else
+		{
+			newLine += line.substr(index, line.length() - 1);
+			return (newLine);
+		}
+	}
+	return (newLine);
 }
 
+int	main(int ac, char **av)
+{
+	std::string line;
+	std::string processedline;
+	std::ifstream inFile;
+	std::string nameOutfile;
+	// A. error handling
+	/*
+	1. wrong No arguments
+	2. infile does not exist or cant be opened
+	3. s1="" and/ s2=""
+	* case for empty file
+	*/
+	if (ac != 4)
+		return (std::cout << "Bad arguments" << std::endl, 1);
+	// B. Open file input and set output
+	inFile.open(av[1]);
+	if (!inFile.is_open())
+		return (std::cout << "Could not open input file" << std::endl, 1);
+	nameOutfile = std::string(av[1]) + ".replace";
+	std::ofstream outFile(nameOutfile.c_str());
+	// LOOP
+	getline(inFile, line);
+	// while (getline(inFile, line))
+	//{
+	std::cout << line << std::endl;
+	processedline = processLine(line, av[2], av[3]);
+	if (outFile.is_open())
+		outFile << processedline << std::endl;
+	else
+		return (std::cout << "Could not open output file" << std::endl, 1);
+	//}
+	// C. Read line
+	// D. Replace words in line
+	// E. Put final line in file
+}
+
+/*
+
+	INPUT: ./ input is cat
+
+	//A. error handling
+		1. wrong No arguments
+		2. infile does not exist or cant be opened
+		3. s1="" and/ s2=""
+		* case for empty file
+
+	if (ac != 4)
+		return (std::cout << "Bad arguments" << std::endl, 1);
+
+	//B. Open file input and set output
+	inFile.open(av[1]);
+	if (!inFile.is_open())
+		return (std::cout << "Could not open input file" << std::endl, 1);
+	nameOutfile = std::string(av[1]) + ".replace";
+	std::ofstream outFile(nameOutfile.c_str());
+	if (!outFile.is_open())
+		return (std::cout << "Could not open output file" << std::endl, 1);
+	//LOOP
+	//C. Read line
+
+	//D. Replace words in line
+	//E. Put final line in file
+*/
---
+++ ex04/main.hpp
@@ -0,0 +1,9 @@
+#ifndef MAIN_HPP
+# define MAIN_HPP
+
+#include <iostream>
+//template<typename T>
+//
+//void print_with_nl(std::string description, T value);
+
+#endif
